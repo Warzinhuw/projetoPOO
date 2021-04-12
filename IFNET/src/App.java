2d9c497b845c39c1cf0264f96b4a891db02f76b2
jooalvaro <joaoalvaro2345@gmail.com>
2021-04-12 20:30:09 -0300

    alterado

--- IFNET/src/App.java
+++ IFNET/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //
-    // abcdefghij ggggg ddddd abffsfsf
+    // abcdefghij ggggg ddddd abffsfsf nnnnnnnnn
     }
 }
